9f8fce7659d7116e8c58ffc1c34a0fd04a34c109
Anurag Kandulna <dev.ak@outlook.in>
2024-06-15 19:32:18 +0530

    added notes for data types

--- test002.js
+++ test002.js
@@ -1,3 +1,17 @@
 "use strict";
 
 // alert(3+3) // in node js this will not work. This syntax only works in browser.
+
+// Data Types:
+// numbers => 2 ^ 53
+// bigint
+// string
+// boolean
+// null => standalone value
+// undefined
+// symbol => will be used in react
+
+// object
+console.log(typeof undefined);
+console.log(typeof null)
+
