750869644242610abf3204bf0ab34cd7060a4111
Anurag Kandulna <dev.ak@outlook.in>
2024-06-15 18:47:34 +0530

    let and const

---
+++ test001.js
@@ -0,0 +1,18 @@
+console.log("nnnn")
+
+const accountId = 12344
+let accountEmail = "ABC@GMAIL.COM"
+var accountPassword = "1234"
+accountCity = "Blr"
+
+// accountId = 12
+
+accountEmail = "ab@ab.com"
+accountPassword = "6666"
+accountCity = "CRJ"
+
+// console.log(accountId);
+/**
+ * Do not use "var" keyword because of block and functional scope.
+ */
+console.table([accountId, accountEmail, accountPassword, accountCity])
