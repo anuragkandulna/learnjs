2ad4cf348cbeecca5a33ecf944a04f2b8ee81695
Anurag Kandulna <dev.ak@outlook.in>
2024-06-15 21:41:51 +0530

    strings data type

--- test002.js
+++ test002.js
@@ -12,18 +12,18 @@
 // symbol => will be used in react
 
 // object
-console.log(typeof undefined);
-console.log(typeof null)
-console.log("Data Types finished ...")
+// console.log(typeof undefined);
+// console.log(typeof null)
+// console.log("Data Types finished ...")
 
 
 // COnversion
 let score = "11a"
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let scoreInNum = Number(score)
-console.log(typeof(scoreInNum))
-console.log(scoreInNum)
+// console.log(typeof(scoreInNum))
+// console.log(scoreInNum)
 
 // "33" => 33
 // "33asc" => NaN
@@ -31,8 +31,14 @@ console.log(scoreInNum)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false; "jshd" => true
 
+// Strings
+const key = new String('Anurag')
+const val = "abckd"
+console.log(typeof key)
+console.log(typeof val)
+console.log(val[1])
