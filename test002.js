1c20b5dc737bb5e4ffcc55db8590b31574bb643e
Anurag Kandulna <dev.ak@outlook.in>
2024-06-15 19:46:09 +0530

    notes for conversion

--- test002.js
+++ test002.js
@@ -14,4 +14,25 @@
 // object
 console.log(typeof undefined);
 console.log(typeof null)
+console.log("Data Types finished ...")
+
+
+// COnversion
+let score = "11a"
+console.log(typeof(score))
+
+let scoreInNum = Number(score)
+console.log(typeof(scoreInNum))
+console.log(scoreInNum)
+
+// "33" => 33
+// "33asc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false; "jshd" => true
 
