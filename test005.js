1409c7278b4cb906f755bf76554f7cdb44bd97e4
Anurag Kandulna <dev.ak@outlook.in>
2024-06-27 15:54:57 +0530

    this keyword

---
+++ test005.js
@@ -0,0 +1,15 @@
+// Arrow functions
+
+const user = {
+    username: "anurag",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} welcome to website`)
+    }
+}
+
+// console.log(user.welcomeMessage())
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
