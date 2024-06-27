eae82ab733197eda0219d09cd790a30920bf283f
Anurag Kandulna <dev.ak@outlook.in>
2024-06-27 17:08:42 +0530

    iify functions

---
+++ test006.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IIFE)
+// To execute function immediatly upon writing definition and isolate the function from global variables pollutions
+
+function printName(){
+    console.log(`Anurag here`)
+}
+printName(); // NOTE: apparantly semi-colon is required here for iify isolation and execution
+
+( (name) => {
+    console.log(`Anurag here ${name}`)
+})('me'); // closing sem-color is a must in iify
