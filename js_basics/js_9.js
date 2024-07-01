2cb95371daa9d1ffe73225a85ce0b08aad03ba2d
Anurag Kandulna <dev.ak@outlook.in>
2024-07-01 12:58:45 +0530

    added accessibility

--- js_basics/js_9.js
+++ js_basics/js_9.js
@@ -2,16 +2,16 @@
 
 const lang = ['java', 'c++', 'python', 'javascript']
 
-lang.forEach(element => {
-    console.log(element)
-});
+// lang.forEach(element => {
+//     console.log(element)
+// });
 
-lang.forEach(function (val) {
-    console.log(val)
+lang.forEach(function (val, index, arr) {
+    console.log(val, index, arr)
 })
 
-function printMe(item){
-    console.log(item)
-}
+// function printMe(item){
+//     console.log(item)
+// }
 
-lang.forEach(printMe)
+// lang.forEach(printMe)
