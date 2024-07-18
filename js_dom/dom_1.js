d36d9d5f55059aaf1331e96107381b87662ba32e
Anurag Kandulna <dev.ak@outlook.in>
2024-07-18 23:39:13 +0530

    linked 3rd file to script

--- js_dom/dom_1.js
+++ js_dom/dom_1.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 const PORT=8080; 
 
-fs.readFile('./js_dom/two.html', function (err, html) {
+fs.readFile('./js_dom/three.html', function (err, html) {
 
     if (err) throw err;    
 
